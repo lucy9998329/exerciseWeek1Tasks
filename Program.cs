672b45dea65fe4b3e1c4d10cbc251120ee46f62b
Luciany Laurentino <lucylaurentino@gmail.com>
2018-03-11 13:07:41 +1300

    more commit

--- Program.cs
+++ Program.cs
@@ -62,23 +62,63 @@ namespace exerciseWeek1Tasks
 
 
             //Task6 - Write a console application that asks the user to enter two numbers, num1 and num2, then swap the two variables and display back to the screen. 
-            //(i.e. test data: Enter num1: 8 Enter num2: 5, Expected output: num1 is 5 num2 is 8) 
-            /*int num1;
-            int num2;
-            string answer = "";
-            Console.Write("Please enter in a number: ");
-            var userInput = Console.ReadLine();
-            if (int.TryParse(username, out num1))
+            //(i.e. test data: Enter num1: 8 Enter num2: 5, Expected output: num1 is 5 num2 is 8)                  
+          /*int num1;
+           int num2;
+           int temp;
+           string answer = "";
+           Console.Write("Please enter in the first number: ");
+           var userInput1 = Console.ReadLine();
+           if (int.TryParse(userInput1, out num1))           
+           {
+           Console.Write("Please enter in the second number: ");
+           var userInput2 = Console.ReadLine();
+           if (int.TryParse(userInput2, out num2))
+           {
+           temp = num1;
+           num1 = num2;
+           num2 = temp;
+           Console.WriteLine(answer += $"You have entered {num1} and {num2}");
+           }  
+           }*/
+
+           //Task8 - Write a console application that asks the user to enter their final course mark and then tells them if they passed or not (pass mark is 50%).
+           /*int grade;
+           string answer = "";
+           Console.Write("Please enter in your final course mark to see your final result: ");
+           var isGrade = int.TryParse(Console.ReadLine(), out grade);
+           if (grade <= 100 && grade >= 50)                      
+               {
+                   answer += $"{grade} = passed";
+               }
+            else if (grade < 50)                      
+               {
+                   answer += $"{grade} = failed";
+               }
+                Console.WriteLine(answer);*/                     
+                      
+
+           //Task9 - Write a console application that contains an array of size 5 and populate with classmates names and display back to the screen.
+           //Hint: Use foreach loop
+            var studentNames = new string[5]{"Aman", "Bodie", "Dylan", "Lucy", "Marisa"};            
+{
+            foreach (var x in studentNames)
             {
-                Console.WriteLine(answer += $"{num2}");
-            }*/
-            
+                Console.WriteLine(x);
+            }
+            //Console.WriteLine(studentNames[0]);
+            //Console.WriteLine(studentNames[1]);
+            //Console.WriteLine(studentNames[2]);
+            //Console.WriteLine(studentNames[3]);
+            //Console.WriteLine(studentNames[4]);    
+}
+
+
             //End the program with blank line and instructions
             Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("Press <Enter> to quit the program");
             Console.ReadKey();
-            
         }
     }
 }
